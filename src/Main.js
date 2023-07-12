6d8a44f38915351675852aeafad1b35f7d2e5d71
ScottCRed <ScottC@redpandasoftware.co.za>
2023-07-12 11:20:52 +0200

    Add current and max score states

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import Footer from "./Footer";
 import GameContainer from "./GameContainer";
 import Header from "./Header";
+import { characters } from "./characters";
 
 function Main () {
+    const [currentScore, setCurrentScore] = useState(0);
+    const [maxScore, setMaxScore] = useState(characters.length)
+
     return (
         <>
-            <Header className="main" id="header" title="Memory Game"/>
+            <Header 
+            className="main" 
+            id="header" 
+            title="Memory Game" 
+            currentScore={currentScore} 
+            maxScore={maxScore}/>
+
             <GameContainer />
-            <Footer className='footer' content='Created by Scott Carroll 2023'/>
+            
+            <Footer 
+            className='footer' 
+            content='Created by Scott Carroll 2023'/>
         </>
     );
 };
