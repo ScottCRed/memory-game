3f0d9532df525a6b3544a909a639020c0364032c
ScottCRed <ScottC@redpandasoftware.co.za>
2023-07-12 11:48:07 +0200

    Add character objects

--- src/characters.js
+++ src/characters.js
@@ -23,4 +23,94 @@ export const characters = [
         id: uniqid(),
         name: "Inoske Hashibira",
     },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/02/X-Great-Cute-Girls-Doing-Weird-Things-Anime-Kanao-Tsuyuri-Flipping-A-Coin-To-Make-Decisions.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Kanao Tsuyuri"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/01/Demon-Slayer---Tanjiro-Looking-At-An-Angry-Genya.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Genya Shinazugawa"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/09/Sanemi-Shinazugawa-full-of-Scars-from-Demon-Slayers.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Sanemi Shinazugawa"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/02/giyu-sword.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Giyu Tomioka"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/04/shinobu-insect-hashira.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Shinobu Kocho"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/03/tengen-weapons.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Tengen Uzui"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/02/Demon-Slayer-Mitsuri-Kanroji-shy.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Mitsuri Kanroji"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/07/obanai-iguro-face-night-edit.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Obanai Iguro"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/11/demon-slayer-Muichiro-Tokito-anime.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Muichiro Tokito"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/11/demon-slayer-Gyomei-Himejima-anime.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Gyomei Himejima"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/03/X-Anime-That-Killed-A-Character-Way-Too-Early-Kyojuro-Rengoku-From-Demon-Slayer.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Kyojuro Rengoku"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/11/demon-slayer-Sabito-anime.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Sabito"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/09/Aoi%C2%A0Kanzaki.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Aoi Kanzaki"
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/11/demon-slayer-Kagaya-Ubuyashiki-anime.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Kagaya Ubuyashiki"
+    },
+
+    {
+        src: "",
+        id: uniqid(),
+        name: ""
+    },
 ]
