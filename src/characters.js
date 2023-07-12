ae0007c28a5f6df0634fcc2d1f61c5f935afeee1
ScottCRed <ScottC@redpandasoftware.co.za>
2023-07-12 10:19:22 +0200

    Remove img file and move characters.js

---
+++ src/characters.js
@@ -0,0 +1,26 @@
+import uniqid from 'uniqid';
+export const characters = [
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/05/collage-maker-12-may-2023-07-31-pm-1616.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Tanjiro Kamado",
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/03/X-Most-Lovable-Anime-Sisters-Ranked-Nezuko-Kamado-(Demon-Slayer).jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Nezuko Kamado",
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/11/zenitsu-6.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Zenitsu Agatsuma",
+    },
+
+    {
+        src: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/03/demon-slayer-inosuke-s-strongest-attacks-abilities-enhanced-sense-of-touch.jpg?q=50&fit=crop&w=1500&dpr=1.5",
+        id: uniqid(),
+        name: "Inoske Hashibira",
+    },
+]
